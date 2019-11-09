44435d8ddc0abf0bab0588fe412ea849c1a8020d
chenzhaolong <1332588507@qq.com>
2019-11-09 21:33:10 +0800

    初步完成前端部分v2

--- client/routes/router.js
+++ client/routes/router.js
@@ -1,2 +1,5 @@
-/** * @file 路由入口文件 */ import Vue from 'vue'; import Router from 'vue-router'; Vue.use(Router); export default new
-Router({ mode: 'history', routes: [] });
+/** * @file 路由入口文件 */
+import Vue from 'vue';
+import Router from 'vue-router';
+Vue.use(Router);
+export default new Router({ mode: 'history', routes: [] });
