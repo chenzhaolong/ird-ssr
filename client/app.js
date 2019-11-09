22a2f9116a9291cdda4e854a16441220f66d44a4
chenzhaolong <1332588507@qq.com>
2019-11-09 21:35:38 +0800

    解决prettier的问题v1

--- client/app.js
+++ client/app.js
@@ -1,3 +1,18 @@
-/** * @file 应用前端入口 */ import Vue from 'vue'; import App from './app.vue'; import router from './routes/router';
-import store from './store/store'; // 创建一个app应用 export function createApp() { const app = new Vue({ ...App, store,
-router }); return {app, router, store}; }
+/**
+ * @file 应用前端入口
+ */
+
+import Vue from 'vue';
+import App from './app.vue';
+import router from './routes/router';
+import store from './store/store';
+
+// 创建一个app应用
+export function createApp() {
+  const app = new Vue({
+    ...App,
+    store,
+    router,
+  });
+  return { app, router, store };
+}
--- client/routes/router.js
+++ client/routes/router.js
@@ -1,5 +1,9 @@
 /** * @file 路由入口文件 */
 import Vue from 'vue';
 import Router from 'vue-router';
+
 Vue.use(Router);
-export default new Router({ mode: 'history', routes: [] });
+export default new Router({
+  mode: 'history',
+  routes: [],
+});
