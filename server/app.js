eb243a59e5e32ea7a05a6f4acd4e2b61c70d1431
chenzhaolong <1332588507@qq.com>
2019-12-15 00:46:16 +0800

    server端的中间件

--- server/app.js
+++ server/app.js
@@ -1,8 +1,24 @@
 /**
- * @file 后端应用
+ * @file 后端应用聚合层
  */
 
+import bodyParse from 'koa-bodyparser';
+import compress from 'koa-compress';
+import staticServer from 'koa-static';
+import SSRRender from './middleware/serverRender';
+
+const path = require('path');
 const Koa = require('koa2');
 const app = new Koa();
+
+// 静态资源
+const staticPath = path.resolve(__dirname, '../');
+const resource = staticServer(staticPath);
+
+// 中间件注册
+app.use(bodyParse);
+app.use(compress);
+app.use(resource);
+app.use(SSRRender);
 
 export default app;
