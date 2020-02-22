3e9688f5218a312964894fafa74b0ce76954fe79
chenzhaolong <1332588507@qq.com>
2020-02-22 17:08:27 +0800

    全局注入this.$request方法

--- client/app.js
+++ client/app.js
@@ -6,6 +6,10 @@ import Vue from 'vue';
 import App from './app.vue';
 import { createRouter } from './routes/router';
 import { createStore } from './store/store';
+import { ClientService } from './service/clientCgiService';
+
+// 全局注入请求调用, 组件可直接调用this.$request
+Vue.use(ClientService.installVue());
 
 // 创建一个app应用
 export function createApp() {
