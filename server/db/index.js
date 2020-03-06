1dd73c91139557114dbc037d3e78039a8f78df64
chenzhaolong <1332588507@qq.com>
2020-03-07 01:26:22 +0800

    采用aop来判断db实例中sql是否存在

--- server/db/index.js
+++ server/db/index.js
@@ -5,11 +5,28 @@ import DBService from './DBService';
 
 export const DBInstance = (function() {
   let instance = null;
+  Function.prototype.before = function(fn) {
+    const self = this;
+    return function() {
+      const sqlExist = fn(this, arguments);
+      if (sqlExist) {
+        return self.apply(this, arguments);
+      }
+    };
+  };
   return function() {
     if (instance) {
       return instance;
     } else {
-      return (instance = new DBService());
+      instance = new DBService();
+      ['createTable', 'insert', 'remove', 'update', 'select'].forEach(
+        method => {
+          instance[method] = instance[method].before(context => {
+            return context.sql && true;
+          });
+        },
+      );
+      return instance;
     }
   };
 })();
