8516917d74026bd060cbaa8756c81215a65ee039
chenzhaolong <1332588507@qq.com>
2020-04-12 15:29:33 +0800

    打包脚本添加版本号升级功能

--- bin/publish.sh
+++ bin/publish.sh
@@ -8,12 +8,20 @@ DLL="NO"
 # m=all：重新编译全部，默认值
 MODE="all"
 
+# param -v: 更新版本号
+# v=patch：迭代和修复类型，只修改package.json中version的最后一位，默认
+# v=minor：大需求类型，只修改package.json中version的中间位
+# v=major：大版本改造类型，只修改package.json中version的中间位
+VERSION="patch"
+
+# 下载依赖
 installNpm() {
   if [ ! \( -d ./node_modules \) ];then
     npm i
   fi
 }
 
+# 生成dll文件
 runDll() {
   if [ $DLL = "YES" ];then
     npm run dll
@@ -24,10 +32,11 @@ runDll() {
   fi
 }
 
+# 编译
 compiler() {
   case $MODE in
     asset)
-       npm run client prod;;
+       npm run client prod
        npm run ssr;;
     *)
       npm run client prod
@@ -36,17 +45,25 @@ compiler() {
   esac
 }
 
+# 迁移文件
 moveFile() {
     cp ./bin/deploy.sh ./output/
     cp ./config/ecosystem.config.js ./output/
     cp ./package.json ./output/
 }
 
+# 打包
 makePack() {
    tar -czvf output.`date +%Y-%m-%d-%H-%M-%S`.tar.gz ./output
    rm -rf ./output
 }
 
+# 更新版本号
+updateVersion() {
+   npm version $VERSION
+}
+
+// 执行发布
 publish() {
    while [ -n "$1" ]
    do
@@ -59,6 +76,11 @@ publish() {
           MODE="$2"
           shift
         fi;;
+     -v)
+         if [ -n "$2" ];then
+           VERSION="$2"
+           shift
+         fi;;
      *) shift;;
     esac
    done
@@ -70,8 +92,9 @@ publish() {
    installNpm
    runDll
    compiler
+   updateVersion
    moveFile
    makePack
 }
 
-publish $1 $2 $3
+publish $1 $2 $3 $4 $5
